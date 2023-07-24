f89c1f8671a589933f73e7bec4a0c802a8da0d16
Samu7712 <Samuelabediako7@gmail.com>
2023-07-24 13:35:58 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * swap_int - Entry point
+  * @a: argument 1
+  * @b: argument 2
+  * return 0
+  */
+
+void swap_int (int *a, int *b)
+{
+int temp = *a;
+*a = *b;
+*b = temp;
+
+
+}
