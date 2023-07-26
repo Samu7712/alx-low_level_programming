93a7bc5a99432fadb4fea5df5ff65097f0062f43
Samu7712 <Samuelabediako7@gmail.com>
2023-07-26 10:43:20 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat - Lets concatenates two strings
+ * @dest: input argument
+ * @src: input argument
+ *
+ * Return: desk
+ */
+char *_strcat(char *dest, char *src)
+{
+int conCat1;
+int conCat2;
+
+conCat1 = 0;
+while (dest[conCat1] != 0)
+{
+conCat1++;
+}
+conCat2 = 0;
+while (src[conCat2] != 0)
+{
+dest[conCat1] = src[conCat2];
+conCat1++;
+conCat2++;
+}
+
+dest[conCat1] = 0;
+return (dest);
+}
